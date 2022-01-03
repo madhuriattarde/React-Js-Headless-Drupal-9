3209a3f5554bc830198f2395bddb41282694239d
madhuriattarde <madhuridttarde@gmail.com>
2022-01-03 11:24:46 -0600

    app changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Articles from "./Components/Article/Articles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import events from 'events';
+import ajax from './ajax';
+
+// Create an emitter object so that we can do pub/sub
+const emitter = new events.EventEmitter();
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      nodes: []
+    }
+    this.refresh = this.refresh.bind(this)
+  }
+
+  componentWillMount() {
+    emitter.addListener('NODE_UPDATED', this.refresh)
+  }
+
+  componentWillUnmount() {
+    emitter.addListener('NODE_UPDATED', this.refresh)
+  }
+
+  async componentDidMount() {
+    await this.refresh()
+  }
+
+  async refresh() {
+    // AJAX fetch server/node/rest?_format=json and setState with the response data
+    try {
+      const axios = await ajax() // wait for an initialized axios object
+      const response = await axios.get('/api/articles') // wait for the POST AJAX request to complete
+      if (response.data) {
+        // setState will trigger repaint
+        this.setState({ nodes: response.data });
+        
+      }
+      } catch (e) {
+      alert(e)
+    }
+  }
+  
+
+  render() {
+    return (
+      <div className="App">
+        <Articles
+          data={this.state.nodes}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
